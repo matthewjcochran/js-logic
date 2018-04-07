83cff4c98f0eec8002425063e286f379852f6641
Matthew Cochran <matthewjcochran@gmail.com>
2018-04-06 21:53:37 -1000

    Big bird problem

--- exercises.js
+++ exercises.js
@@ -108,7 +108,13 @@ console.log(mustBeTrue('true'));
  * Console.log your result.
 */
 
+function bigBird(word){
+  if(word.length === 3){
+    return 'Word to Big Bird!';
+  }
+}
 
+console.log(bigBird('abcd'));
 
 /*
  * #6
